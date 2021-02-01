c290309b5d3cfd63220dcb56e587f6f40025351a
goenning <me@goenning.net>
2021-02-01 19:56:09 +0000

    progress

---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <main>
+      <p>You are on the <strong>Contact</strong> page. Go back home to learn more about this site.</p>
+    </main>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,14 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Cookieless Google Analytics Example App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Cookieless Google Analytics Example App
-        </h1>
-        <p>Open your Dev console and try to find any cookie created by this website. You won't!</p>
-        <p>
-          <Link href="/about"><a className={styles.a}>Go to /about</a></Link>
-        </p>
-        <footer className={styles.footer}>This site is open source: <a className={styles.a} href="https://github.com/goenning/cookieless-ga">https://github.com/goenning/cookieless-ga</a></footer>
-      </main>
-    </div>
+    <main>
+      <p>This page is an example of how to setup a site with Google Analytics without Cookies.</p>
+      <p>Open your Dev console and try to find any cookie created by this website. You won't!</p>
+      <br />
+      <p>Navigate to other pages and notice how page view events are sent to Google on your network tab.</p>
+      <br />
+      <p>Although this site is built with Next.js, the same setup can be applied to any language or framework.</p>
+      <br />
+      <p>To learn how it works, read this blog post <a target="_blank" href="https://goenning.net">https://goenning.net</a></p>
+    </main>
   )
 }
